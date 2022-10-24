daf57dde7d29e2d49b169ee99ec973cc4cee94e3
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2022-10-24 13:28:47 +0100

    move package installs to zzz.R

--- scripts/main.R
+++ scripts/main.R
@@ -1,35 +1,6 @@
 
 # Example script for blendR
 
-
-if (.Platform$OS.type == "windows") {
-  if ("survHE" %in% rownames(installed.packages()) == FALSE) {
-    install.packages("survHE",
-                     repos = c("http://www.statistica.it/gianluca/R",
-                               "https://cran.rstudio.org",
-                               "https://inla.r-inla-download.org/R/stable"),
-                     dependencies = TRUE)
-  }
-}
-
-if (.Platform$OS.type == "windows") {
-  if ("INLA" %in% rownames(installed.packages()) == FALSE) {
-
-    if (!require("BiocManager", quietly = TRUE))
-      install.packages("BiocManager")
-
-    BiocManager::install("graph")
-    BiocManager::install("Rgraphviz")
-
-    # requires the latest version of R
-    install.packages("INLA",
-                     repos = c(getOption("repos"),
-                               INLA = "https://inla.r-inla-download.org/R/stable"),
-                     dep = TRUE)
-  }
-}
-
-
 library(survHE)
 library(INLA)
 
