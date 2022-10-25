3a9c51715dc35fe547c663028f22aa3e7aa15e8e
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2022-10-25 08:40:04 +0100

    rename ext_surv_sim

--- R/ext_surv_sim.R
+++ R/ext_surv_sim.R
@@ -7,25 +7,34 @@
 #' @param n       The number of patients to construct the artificial external dataset; default 70
 #'
 #' @return
-ext_surv_sim <- function(t_info, S_info, T_max){
+#' @examples
+#' dat <- ext_surv_sim(t_info = c(10,20,50),
+#'                     S_info = c(0.9, 0.8, 0.2),
+#'                     T_max = 100, n = 100)
+#' km_trt_fit <- survfit(Surv(time, event) ~ 1, data = dat)
+#' plot(km_trt_fit)
+#'
+ext_surv_sim <- function(t_info, S_info, T_max, n = 100) {
   set.seed(1996)
+
+  ## length(t_info) == length(S_info)?
 
   # Partition the time horizon into intervals
   S <- c(1, S_info, 0)
   t <- c(0, t_info, T_max)
 
-  c <- length(S) - 1
-  d <- length(t)
+  n_S <- length(S)
+  n_t <- length(t)
 
-  n_par <- vector(mode = "numeric", length = c)
+  n_par <- vector(mode = "numeric", length = n_S - 1)
 
-  for (i in seq_len(c)) {
+  for (i in seq_len(n_S - 1)) {
     n_par[i] <- round(n*(S[i] - S[i+1]), digits = 0)
   }
 
   n_sim <- sum(n_par)
 
-  min_unif <- rep(t[-d], n_par)
+  min_unif <- rep(t[-n_t], n_par)
   max_unif <- rep(t[-1], n_par)
 
   # Create survival times using uniform distribution
