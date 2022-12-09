4b25b26fe29a22673f1c0cb970b3d098ca9c6c67
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2022-12-09 14:59:21 +0000

    first attempt at using input external data format from Chris's survextrap package

---
+++ R/transform_extdata.R
@@ -0,0 +1,31 @@
+
+#' Transform external data from risk set format
+#'
+#' @param extdat External data; data frame
+#' @param tmax Maximum survival time
+#'
+#' @return Survival probabilities
+#' @export
+#'
+#' @examples
+#' extdat <- data.frame(start = c(5, 10, 15, 20),
+#'                      stop =  c(10, 15, 20, 25),
+#'                      n = c(100, 100, 100, 100),
+#'                      r = c(50, 40, 30, 20))
+#' transform_extdata(extdat, tmax = 100)
+#'
+transform_extdata <- function(extdat, tmax = 100) {
+browser()
+
+  extdat <- extdat |>
+    mutate(prop = r/n,
+           pcum = cumprod(prop))
+
+  # need to assume something about the absolute survival probabilities
+  # assume linear i.e maximum survival
+
+  surv <- 1 - (1/tmax)*extdat$start[1]
+
+  data.frame(s = c(surv, surv*extdat$pcum),
+             t = c(extdat$start[1], extdat$stop))
+}
