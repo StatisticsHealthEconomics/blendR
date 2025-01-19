86e5201c6a09306526c22aecd31a47babdce0a97
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2025-01-19 17:01:14 +0000

    trying to get the Checks to pass for CRAN

--- R/data_times.R
+++ R/data_times.R
@@ -5,29 +5,6 @@
 #' @return A vector of event times
 #' @keywords internal
 #'
-#' @examplesIf rlang::is_installed("survHEhmc")
-#' library(survHE)
-#'
-#' ## trial data
-#' data("TA174_FCR", package = "blendR")
-#'
-#' ## externally estimated data
-#' data_sim <- ext_surv_sim(t_info = 144,
-#'                          S_info = 0.05,
-#'                          T_max = 180)
-#'
-#' obs_Surv <- fit.models(formula = Surv(death_t, death) ~ 1,
-#'                        data = dat_FCR,
-#'                        distr = "exponential",
-#'                        method = "hmc")
-#'
-#' ext_Surv <- fit.models(formula = Surv(time, event) ~ 1,
-#'                        data = data_sim,
-#'                        distr = "exponential",
-#'                        method = "hmc")
-#'
-#' c(data_times(obs_Surv), data_times(ext_Surv))
-#'
 data_times <- function(S)
   UseMethod("data_times", S)
 
